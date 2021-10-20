b962d4d17e2f05f2fbe466318c0446c87b0df82d
Varun Brahmane <varunbrahmane30@gmail.com>
2021-10-20 19:03:56 +0530

    Added Envirenment base URL.

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: false,
-  BACKEND_BASE_URL: 'https://new-bookstore-backend.herokuapp.com',
+  BACKEND_BASE_URL: 'https://bookstore.incubation.bridgelabz.com',
 };
