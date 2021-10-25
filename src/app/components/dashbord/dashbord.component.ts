8238076c2213f0f19c13baa19b98acaaa6825a62
Varun Brahmane <varunbrahmane30@gmail.com>
2021-10-25 21:28:06 +0530

    added: dashbord component footer. and page responsive alignments.

--- src/app/components/dashbord/dashbord.component.ts
+++ src/app/components/dashbord/dashbord.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-dashbord',
   templateUrl: './dashbord.component.html',
-  styleUrls: ['./dashbord.component.scss']
+  styleUrls: ['./dashbord.component.scss'],
 })
 export class DashbordComponent implements OnInit {
+  constructor(private router: Router) {}
 
-  constructor() { }
+  ngOnInit(): void {}
 
-  ngOnInit(): void {
+  cartbtn() {
+    this.router.navigateByUrl('home/cart');
   }
-
+  wishlist() {
+    this.router.navigateByUrl('home/wishlist');
+  }
+  myOrders() {
+    this.router.navigateByUrl('home/myorders');
+  }
 }
