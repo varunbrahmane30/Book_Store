be446f6987675787f4ce9551c26e731780f80e04
Varun Brahmane <varunbrahmane30@gmail.com>
2021-10-25 21:41:46 +0530

    added:  http services of get, post , put , and delete api with changed header.

--- src/app/service/httpservice/httpservice.service.ts
+++ src/app/service/httpservice/httpservice.service.ts
@@ -7,19 +7,50 @@ import { Injectable } from '@angular/core';
 export class HttpserviceService {
   constructor(private httpClient: HttpClient) {}
 
-  GetService(url = '', headers: any = null) {
-    return this.httpClient.get(url, { headers });
+  GetService(url = '', token: boolean = false, headers: any = null) {
+    return this.httpClient.get(url, token && headers);
   }
+
+  // PostService(
+  //   URL: string,
+  //   payload: any,
+  //   token: boolean = false,
+  //   header: any = null
+  // ) {
+  //   return this.httpClient.post(URL, payload, token && header);
+  // }
 
   PostService(url = '', payload = null, headers: any = null) {
     return this.httpClient.post(url, payload, { headers });
+  }
+
+  postService(
+    URL: string,
+    payload: any,
+    token: boolean = false,
+    header: any = null
+  ) {
+    return this.httpClient.post(URL, payload, token && header);
   }
 
   PutService(url = '', payload = null, headers: any = null) {
     return this.httpClient.put(url, payload, { headers });
   }
 
+  putService(
+    URL: string,
+    payload: any,
+    token: boolean = false,
+    header: any = null
+  ) {
+    return this.httpClient.put(URL, payload, token && header);
+  }
+
   DeleteService(url = '', headers: any = null) {
     return this.httpClient.delete(url, { headers });
+  }
+
+  deleteService(URL: string, token: boolean = false, header: any = null) {
+    return this.httpClient.delete(URL, token && header);
   }
 }
