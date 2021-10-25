db0bb8f6829a2767894cdf1676d09ba58b0201dc
Varun Brahmane <varunbrahmane30@gmail.com>
2021-10-25 21:43:05 +0530

    added: wishlist service for get all wishlist and delete wishlist and add wishlist item.

---
+++ src/app/service/WishlistService/wishlist.service.ts
@@ -0,0 +1,61 @@
+import { HttpHeaders } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+import { HttpserviceService } from '../httpservice/httpservice.service';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class WishlistService {
+  private BACKEND_BASE_URL = environment.BACKEND_BASE_URL;
+  token: any;
+  constructor(private http: HttpserviceService) {
+    this.token = localStorage.getItem('token');
+  }
+
+  addItem(productID: any) {
+    let header = {
+      headers: new HttpHeaders({
+        'x-access-token': this.token,
+        'Content-Type': 'application/json',
+      }),
+    };
+    return this.http.postService(
+      this.BACKEND_BASE_URL + '/bookstore_user/add_wish_list/' + productID,
+      null,
+      true,
+      header
+    );
+  }
+
+  getItems() {
+    let header = {
+      headers: new HttpHeaders({
+        'x-access-token': this.token,
+        'Content-Type': 'application/json',
+      }),
+    };
+
+    return this.http.GetService(
+      this.BACKEND_BASE_URL + '/bookstore_user/get_wishlist_items',
+      true,
+      header
+    );
+  }
+
+  removeItem(productID: any) {
+    let header = {
+      headers: new HttpHeaders({
+        'x-access-token': this.token,
+        'Content-Type': 'application/json',
+      }),
+    };
+    return this.http.deleteService(
+      this.BACKEND_BASE_URL +
+        '/bookstore_user/remove_wishlist_item/' +
+        productID,
+      true,
+      header
+    );
+  }
+}
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,6 @@
 export const environment = {
   production: false,
-  BACKEND_BASE_URL: 'https://bookstore.incubation.bridgelabz.com',
+  // BACKEND_BASE_URL: 'https://bookstore.incubation.bridgelabz.com',
+
+  BACKEND_BASE_URL: 'https://new-bookstore-backend.herokuapp.com',
 };
