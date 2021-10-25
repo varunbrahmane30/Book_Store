e7bf3ae0ef850b9eb37e5df4d7a1d7b6e840cb18
Varun Brahmane <varunbrahmane30@gmail.com>
2021-10-25 21:29:19 +0530

    added: get all books pagination bar and page responsive code alignment.

--- src/app/components/getallbooks/getallbooks.component.ts
+++ src/app/components/getallbooks/getallbooks.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { BookstoreService } from 'src/app/service/BookService/bookstore.service';
+import { Router } from '@angular/router';
+import { DataService } from 'src/app/service/dataservice/data.service';
+
 @Component({
   selector: 'app-getallbooks',
   templateUrl: './getallbooks.component.html',
@@ -7,10 +10,37 @@ import { BookstoreService } from 'src/app/service/BookService/bookstore.service'
 })
 export class GetallbooksComponent implements OnInit {
   books: any;
+  pageBooks: any;
   bookscount: any;
   selectedValue: any;
   box: any;
-  constructor(private bookservice: BookstoreService) {}
+
+  length: number;
+  pageSize: number;
+  pageIndex: number;
+
+  constructor(
+    private bookservice: BookstoreService,
+    private _route: Router,
+    private _dataservice: DataService
+  ) {
+    this.length = 200;
+    this.pageIndex = 1;
+    this.pageSize = 12;
+  }
+
+  public handlePage(e: any) {
+    this.pageIndex = e.pageIndex;
+    this.pageSize = e.pageSize;
+    this.iterator();
+  }
+
+  private iterator() {
+    const end = (this.pageIndex + 1) * this.pageSize;
+    const start = this.pageIndex * this.pageSize;
+    const part = this.books.slice(start, end);
+    this.pageBooks = part;
+  }
 
   ngOnInit(): void {
     this.displaybooks();
@@ -27,8 +57,17 @@ export class GetallbooksComponent implements OnInit {
         console.log(response.result, response.result.length);
         this.books = response.result;
         this.bookscount = response.result.length;
+        this.length = this.bookscount;
+        this.iterator();
       },
       (error: any) => console.log(error)
     );
+  }
+
+  displayselected(book: any) {
+    this._route.navigateByUrl('/home/bookdetail');
+    this._dataservice.changeMessage(book);
+
+    console.log(book);
   }
 }
